4f12b59c94a962d622fef119419001e976387b84
Kartikay <bhardwajkartikay489@gmail.com>
2026-03-06 18:16:08 +0530

    Update API's URL

--- Frontend/src/features/user/services/user.api.js
+++ Frontend/src/features/user/services/user.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/user",
+  baseURL: "https://instagrow-h9rx.onrender.com/api/user",
   withCredentials: true,
 });
 
